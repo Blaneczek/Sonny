563f28ee5ea58fddc1bb603f9eac351c4fa437c4
Blaneczek <dawid.szoldra17@gmail.com>
2024-12-28 11:47:57 +0100

    dream setup

---
+++ Source/Sonny/PlayerControllers/SNDreamPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SNDreamPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONNY_API ASNDreamPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
